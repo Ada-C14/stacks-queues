30dab5b663099940e96cc09982525d600c134a5a
PaulineChane <pauline.chane@gmail.com>
2021-03-14 19:38:13 -0700

    stack passing tests

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,8 +1,14 @@
 class Queue
 
+  MAX_BUFFER = 20 # max items in queue at a given time
+
+  # Time Complexity: O(1), always initializing constant elements
+  # Space Complexity: O(1), always initializing same number of elements
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = [] # circular buffer
+    @head_index = 0 # track index of head
+    @tail_index = 0 # track index of tail
+    @size = 0 # track size
   end
 
   def enqueue(element)
