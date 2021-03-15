5d28cd5be658a27556a70d06a0f0867860cc1242
PaulineChane <pauline.chane@gmail.com>
2021-03-14 23:57:25 -0700

    balanced passed with time/space complexity

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,27 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n) -- number of characters to check depends on number of characters 
+# in string
+
+# Space Complexity: O(n) -- worst case is that Stack expands to include every character of 
+# input string, which depends on num chars in input string
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  # hashes to track braces
+  close_to_open = {'}' => '{', ')' => '(', ']' => '['}
+  open_to_close = {'{' => '}', '(' => ')', '[' => ']'}
+  # brace stack
+  stack = Stack.new
+
+  string.each_char do |c| # this automatically iterates in order i think
+    if open_to_close[c]
+      stack.push(c)
+    elsif close_to_open[c]
+      get_top = stack.pop
+      return false if close_to_open[c] != get_top # must match brace
+    end 
+  end
+
+  return stack.empty?
 end
 
 # Time Complexity: ?
