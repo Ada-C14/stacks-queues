137a37bc7ff221c40b14953a602c04caba81313e
Genevieve Neely <genevieve.hood@gmail.com>
2021-03-27 13:59:56 -0700

    balanced & postfix_expression

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,55 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string.empty?
+
+  open = {
+    '{' => '}',
+    '[' => ']',
+    '(' => ')'
+  }
+  close = {
+    '}' => '{',
+    ']' => '[',
+    ')' => '('
+  }
+  stack = Stack.new()
+
+  string.each_char do |c|
+    if open.has_key?(c)
+      stack.push(c)
+    elsif close.has_key?(c)
+      brace = stack.pop
+      return false if brace != close[c]
+    else
+      return false # String has an invalid character
+    end
+  end
+  return stack.empty?
 end
 
-# Time Complexity: ?
+# Time Complexity: O(n)
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new()
+  operators = {
+    '*' => true,
+    '+' => true,
+    '-' => true,
+    '/' => true
+  }
+
+  postfix_expression.each_char do |c|
+    if operators[c]
+      y = stack.pop.to_i
+      x = stack.pop.to_i
+      result = x.method(c).(y)
+      stack.push(result)
+    else
+      stack.push(c)
+    end
+  end
+  return stack.pop
 end
