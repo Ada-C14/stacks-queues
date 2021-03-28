9ccf26c5b65914a843f586ad6a4eab069b98dfe1
Tram Bui <pbui17@cmc.edu>
2021-03-27 19:24:43 -0500

    completed balanced method in problem

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,26 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string.empty?
+
+  parens_hash = {"{" => "}",
+    "[" => "]",
+    "(" => ")"
+  }
+
+  stack = Stack.new
+
+  string.each_char do |char|
+    # check if it's an open paren 
+    if parens_hash.include?(char)
+      stack.push(parens_hash[char])
+    else # it's a closed paren
+      paren = stack.pop
+      return false if char != paren
+    end
+  end
+
+  return stack.empty?
 end
 
 # Time Complexity: ?
