ebde57ab57685bce4f957c006ab0a972b4c88c1d
dnsrocha <drocha@gmail.com>
2021-04-08 16:05:27 -0700

    stacks and queues done

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,26 @@
 class Stack
   def initialize
     # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
+    # raise NotImplementedError, "Not yet implemented"
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @store.add_first(element)
+    
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?
+
+    item = @store.remove_first
+
+    return item
+    
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
