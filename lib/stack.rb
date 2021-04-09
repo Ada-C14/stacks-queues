fae1014054ea9b614776dd6422b60500c70eba0d
gessica <gessicamohr18@gmail.com>
2021-04-08 21:15:34 -0700

    all methods for stack are passing

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,25 @@
 class Stack
   def initialize
     # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    @store.add_first(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+  #   raise NotImplementedError, "Not yet implemented"
+    return nil if self.empty?
+    item = @store.remove_first
+    return item
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
