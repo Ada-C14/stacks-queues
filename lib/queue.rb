f0aaab4ef20a2274a1e287007f099d3cce08aa17
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-04-08 23:30:01 -0700

    added methods for stack and queue and passed tests

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,46 @@
 class Queue
-
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = []
+    @front = nil
+    @back = nil
+    @size = 20
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front.nil? 
+      @front = @back = 0
+    elsif (@front - @back == 1) || (@back - @front == @size - 1)
+      raise ArgumentError.new("Array is full") 
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % @size
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front.nil? || @front == @back 
+      raise ArgumentError.new("Array is empty")
+    end
+
+    temp = @store[@front]
+    @front = (@front + 1) % @size
+    return temp
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @back >= @front ? (@back - @front) : (@size - @front + @back)
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front.nil? || @front == @back
   end
 
   def to_s
-    return @store.to_s
+    temp = (@back >= @front) ? @store[@front...@back] : @store[@front...@size].concat(@store[0...@back])
+    return temp.to_s
   end
 end
