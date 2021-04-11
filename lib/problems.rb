bce4a94dc737c45c3b3fe74a2401edf00829251e
Jasmine <jasylop@gmail.com>
2021-04-10 23:02:24 -0700

    implemented balanced method & imported problems to test helper

--- lib/problems.rb
+++ lib/problems.rb
@@ -3,7 +3,36 @@ require_relative './stack.rb'
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  brackets = {
+    "(" => ")",
+    "{" => "}",
+    "[" => "]"
+  }
+
+  openers = brackets.keys
+  closers = brackets.values
+
+  opener_stack = []
+
+  string.each_char do |char|
+    if openers.include?(char)
+      opener_stack.push(char)
+    end
+
+    if closers.include?(char)
+      if opener_stack.empty?
+        return false
+      else
+        removed_opener = opener_stack.pop
+        if char != brackets[removed_opener]
+          return false
+        end
+      end
+    end
+  end
+
+  return opener_stack.empty?
+
 end
 
 # Time Complexity: ?
