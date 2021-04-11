9fa555be0abc40ebb62305b31a2c507cd19eaedc
Ting-Yi Liu <46391352+ichbinorange@users.noreply.github.com>
2021-04-10 23:49:25 -0700

    passed tests for balanced

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,21 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n), it has to run thru the whole string to check the balance, so it's O(n).
+# Space Complexity: O(n), the worst case scenario is to store all the chars in the string to the linked list, so it's O(n).
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  opening = {"{" => true, "[" => true, "(" => true}
+  closing = {"}" => "{", "]" => "[", ")" => "("}
+  result = Stack.new
+
+  string.each_char do |char|
+    if opening[char]
+      result.push(char)
+    elsif closing[char]
+      return false if result.pop != closing[char]
+    end
+  end
+
+  return result.empty?
 end
 
 # Time Complexity: ?
