7335475994eed954fd0d1a65d0c99870b79dce29
Yingjing Lin <yjlin789@gmail.com>
2021-04-12 21:22:40 -0700

    complete stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,36 @@
+require_relative './linked_list.rb'
+
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @head = nil
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    new_node = Node.new(element)
+    new_node.next = @head
+
+    @head.previous = new_node unless @head.nil?
+    @head = new_node
+    if @tail.nil?
+      @tail = @head
+    end
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    raise ArgumentError, "Empty" if self.empty?
+
+      value = @head.data
+      @head = @head.next
+      @head.previous = nil unless @head.nil?
+      return value
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    if @head.nil?
+      return true
+    else
+      return false
+    end
   end
 
   def to_s
