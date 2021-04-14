9c056c2383c3e95c7906e9d79baf4835cbdac508
gessica <gessicamohr18@gmail.com>
2021-04-13 22:28:15 -0700

    balanced problem passes all tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,40 @@
 require_relative './stack.rb'
 
+
 # Time Complexity: ?
 # Space Complexity: ?
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  # raise NotImplementedError, "Not implemented yet"
+  return true if string.empty?
+
+  stack = Stack.new()
+
+  open = {
+    '(' => ')',
+    '{' => '}',
+    '[' => ']'
+  }
+
+  close = {
+    ')' => '(',
+    '}' => '{',
+    ']' => '['
+  }
+
+  string.each_char do |char|
+    if open[char]
+      stack.push(char)
+    elsif close[char] != stack.pop
+      return false
+    end
+  end
+
+  return stack.empty?
+  
 end
 
 # Time Complexity: ?
 # Space Complexity: ?
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  # raise NotImplementedError, "Not implemented yet"
 end
