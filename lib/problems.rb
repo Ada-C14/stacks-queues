65023cb1700933ce3084221e633d0750819593a8
beauttie <15842537+beauttie@users.noreply.github.com>
2021-04-13 21:39:30 -0700

    Implement balanced to pass tests

--- lib/problems.rb
+++ lib/problems.rb
@@ -1,9 +1,24 @@
 require_relative './stack.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(n)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  stack = Stack.new
+  string.each_char do |char|
+    if char == "(" || char == "[" || char == "{"
+      stack.push(char)
+    elsif char == ")" || char == "]" || char == "}"
+      last_pushed = stack.pop()
+      if last_pushed == "(" && char != ")" || 
+        last_pushed == "[" && char != "]" ||
+        last_pushed == "{" && char != "}"
+        return false
+      end
+    end 
+  end
+
+  # check if stack is full of opening braces
+  return stack.empty?
 end
 
 # Time Complexity: ?
