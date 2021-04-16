64d49393d751a8e455cb3b36feb7584d81bf4a0d
Blaine <b.atsbeha@gmail.com>
2021-04-15 18:42:16 -0700

    stack

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,19 +1,23 @@
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
+    # raise NotImplementedError, "Not yet implemented"
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    # element into the store
+    @store.add_first(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    @store.remove_first
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    # raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
