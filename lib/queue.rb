ccf2b135af065e21b8600403202e3a744c4c8ba6
Ren Carothers <rencarothers@gmail.com>
2021-04-16 18:05:34 -0700

    first pass

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,75 @@
 class Queue
 
+  MAX_SIZE = 20
+
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new
+    @front = @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if (@front == 0 && @back == MAX_SIZE - 1) || (@back == (@front -1) % MAX_SIZE - 1)
+      raise Exception.new("Queue is full")
+    elsif @store.empty? # queue is empty
+      @front = 0
+      @back = 0
+    elsif @back == (MAX_SIZE - 1) && @front != 0 # needs to wrap around
+      @back = 0
+    else # otherwise, ok to add
+      @back += 1
+    end
+
+    @store[@back] = element # in any non exception case, add element to queue
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    # removes and returns a value from the front of the queue
+    # front index increases by one
+    # add one then mod length of list bc when we pass the end itll bring us back to zero
+    raise Exception.new("Queue is empty") if @store.empty?
+
+    num_to_dequeue = @store[@front] # save the num to dequeue
+    @store[@front] = nil # overwrite element to delete
+
+    if @front == @back # queue is now empty
+      @front = @back = -1
+    elsif @front + 1 == MAX_SIZE
+      @front = 0
+    else # otherwise, add to front
+      @front += 1
+    end
+
+    return num_to_dequeue
+
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return 0 if @store.empty?
+
+    return (@front - @back).abs + 1
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == @back
   end
 
   def to_s
-    return @store.to_s
+    list = Array.new
+
+    @store[@front...MAX_SIZE].each do |element|
+      list.push(element) if element
+    end
+
+    if @back < @front # the queue has wrapped around
+      @store[0..@back].each do |element|
+        list.push(element) if element
+      end
+    end
+
+    return list.to_s
   end
 end
