7e865aad7ab33328ae75d86133842d443244df01
Taylor Tompkins <taylormililani@gmail.com>
2021-04-19 23:13:45 -0700

    done yayyyy

--- lib/stack.rb
+++ lib/stack.rb
@@ -1,22 +1,24 @@
+require_relative 'linked_list'
+
 class Stack
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = LinkedList.new
   end
 
   def push(element)
-    raise NotImplementedError, "Not yet implemented"
+    @store.add_last(element)
   end
 
   def pop
-    raise NotImplementedError, "Not yet implemented"
+    @store.remove_last if !@store.empty?
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @store.empty?
   end
 
   def to_s
     return @store.to_s
   end
 end
+
