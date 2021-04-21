8bcf829df0df95316e70b155348a6751a33eee9d
Noor B.A <noor.bahr.aluloom@gmail.com>
2021-04-20 20:36:03 -0700

    finalized wave 2

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,57 @@
 class Queue
+  Array_Length = 20
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @store = Array.new(Array_Length)
+    @front = @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if @front.nil? 
+      @front = @back = 0
+    elsif (@front - @back == 1) || (@back - @front == Array_Length - 1)
+      raise ArgumentError, 'Queue is full' 
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % Array_Length
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if @front == @back || @front.nil?
+      raise ArgumentError, 'Queue is empty'
+    end
+
+    temp = @store[@front]
+    @front = (@front + 1) % Array_Length
+    return temp
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return 0 if @front == -1
+    if @front < @back
+      @back - @front + 1
+    else
+      Array_Length - @front + @back
+    end
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front.nil? || @front == @back
   end
 
   def to_s
-    return @store.to_s
+    results = []
+    if @back >= @front 
+      results = @store[@front...@back]
+    else
+      results = @store[@front...Array_Length] + (@store[0...@back])
+    end
+    return results.to_s
   end
+  
 end
