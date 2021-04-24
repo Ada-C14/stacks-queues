b62629ea9231a560785be1cdcd5b534ef5b1a8bc
Beatrice T <be@trice.codes>
2021-04-24 12:44:44 -0700

    finished assignment

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,68 @@
 class Queue
 
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @size = 10
+    @store = Array.new(@size)
+    @front = 0
+    @back = 0
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    # Check for full
+   if @front == (@back + 1) % @size
+      self.expand
+    end
+
+    @store[@back] = element
+    @back = (@back + 1) % @size
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+    if self.empty?
+      return nil
+    end
+
+    value = @store[@front]
+    @front = (@front + 1) % @size
+    return value
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @size
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return (@front == @back)
+  end
+
+  def expand
+    # Initialize new array
+    @new_store = []
+    new_back = 0
+
+    # Dequeue elements into new circular buffer
+    value = self.dequeue
+    while value
+      new_back += 1
+      @new_store.push(value)
+      value = self.dequeue
+    end
+
+    # Set new front back and size
+    @front = 0
+    @back = new_back
+    @size *= 2
+
+    # Pad new blank elements to fill out new size of buffer
+    @new_store += Array.new(@new_store.length)
+    @store = @new_store
   end
 
   def to_s
-    return @store.to_s
+    return @store[@front..(@back-1)].to_s
   end
 end
