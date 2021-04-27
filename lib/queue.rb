20fdb82659e860e6a90a335d6b1a86b27c355767
Ana <anakp07@gmail. omgit config --global user.email anakp07@gmail.com>
2021-04-26 23:37:28 -0700

    added  front, empty methods, test passing

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,12 +1,32 @@
 class Queue
 
-  def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+  ARRAY_SIZE = 20 
+
+  def initialize(size = 20)
+    @store = Array.new(ARRAY_SIZE)
+    @front = @back = 0 
+  
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    
+    # # Empty 
+    # if @front == (@back +1 ) % @queue_size
+    #   riase StandardError, ' Queue is now full'
+    # else 
+    #   @store[]
+    #   @front = 0
+    #   @back = 0 
+
+    # #Full
+    # elsif @front == @back
+    #   length = @store.length
+    #   new_array = Array.new(length  * 2)
+     
+    #   write = 0
+    #   read = @front
+
+    #   while 
   end
 
   def dequeue
@@ -14,7 +34,7 @@ class Queue
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @store[@front]
   end
 
   def size
@@ -22,7 +42,7 @@ class Queue
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == @back
   end
 
   def to_s
