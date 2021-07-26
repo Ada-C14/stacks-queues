db09eff41ee1cca97dd6a6a8f9a0dc37ea9c6aed
Hanh Solo <minhhanhnyc@gmail.com>
2021-07-25 18:44:50 -0700

    submissions

--- lib/queue.rb
+++ lib/queue.rb
@@ -1,31 +1,60 @@
 class Queue
-
+  MAX_SIZE = 20
   def initialize
-    # @store = ...
-    raise NotImplementedError, "Not yet implemented"
+    @list = Array.new(MAX_SIZE)
+    @front = -1
+    @back = -1
   end
 
   def enqueue(element)
-    raise NotImplementedError, "Not yet implemented"
+    if (@back == MAX_SIZE - 1 && @front == 0) || (@back == (@front - 1) % (MAX_SIZE - 1))
+      raise "queue is full"
+    elsif @front == -1 #  queue is empty?
+      @front = 0
+      @back = 1 
+      @list[@front] = element
+    else
+      @list[@back] = element
+      @back = (@back + 1) % MAX_SIZE
+    end
+
   end
 
   def dequeue
-    raise NotImplementedError, "Not yet implemented"
+
+    if @front == -1
+      raise "queue is empty"
+    end
+
+    data = @list[@front]
+
+    if @front == @back  # if the queue is now empty
+      @front = -1
+      @back = -1
+    else
+      @front = (@front + 1) % MAX_SIZE
+    end
+
+    return data
   end
 
   def front
-    raise NotImplementedError, "Not yet implemented"
+    return @list[@front]
   end
 
   def size
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1 ? 0 : @back - @front + 1
   end
 
   def empty?
-    raise NotImplementedError, "Not yet implemented"
+    return @front == -1 || @front == @back
   end
 
   def to_s
-    return @store.to_s
+    if @front <= @back 
+      return @list.slice(@front...@back).to_s 
+    else
+      return (@list.slice(@front...MAX_SIZE) + @list.slice(0...@back)).to_s
+    end
   end
 end
