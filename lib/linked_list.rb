4fd199e28c3eea5bf95e3a83f6b8173e52b1b131
Sophia Donan <sophiadonan@gmail.com>
2021-07-25 21:20:23 -0700

    completed stacks and queues

--- lib/linked_list.rb
+++ lib/linked_list.rb
@@ -1,7 +1,6 @@
-# Defines a node in the singly linked list
 class Node
-  attr_reader :data # allow external entities to read value but not write
-  attr_accessor :next, :previous # allow external entities to read or write next node
+  attr_reader :data 
+  attr_accessor :next, :previous 
 
   def initialize(value, next_node = nil, previous_node = nil)
     @data = value
@@ -10,15 +9,12 @@ class Node
   end
 end
 
-# Defines the singly linked list
 class LinkedList
     def initialize
-      @head = nil # keep the head private. Not accessible outside this class
+      @head = nil
       @tail = nil
     end
 
-    # method to add a new node with the specific data value in the linked list
-    # insert the new node at the beginning of the linked list
     # Time Complexity: O(1)
     # Space Complexity O(1)
     def add_first(value)
@@ -45,8 +41,6 @@ class LinkedList
       return @head.nil?
     end
 
-    # method to find if the linked list contains a node with specified value
-    # returns true if found, false otherwise
     # Time Complexity: O(n)
     # Space Complexity: O(1)
     def search(value)
@@ -59,8 +53,6 @@ class LinkedList
       end
     end
 
-    # method to return the max value in the linked list
-    # returns the data value and not the node
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def find_max
@@ -74,8 +66,6 @@ class LinkedList
       return max
     end
 
-    # method to return the min value in the linked list
-    # returns the data value and not the node
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def find_min
@@ -91,8 +81,6 @@ class LinkedList
       return min
     end
 
-
-    # method that returns the length of the singly linked list
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def length
@@ -107,9 +95,6 @@ class LinkedList
       
     end
 
-    # method that returns the value at a given index in the linked list
-    # index count starts at 0
-    # returns nil if there are fewer nodes in the linked list than the index value
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def get_at_index(index)
@@ -123,7 +108,6 @@ class LinkedList
       end
     end
 
-    # method to print all the values in the linked list
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def visit
@@ -134,8 +118,6 @@ class LinkedList
       end
     end
 
-    # method to delete the first node found with specified value
-    # Time Complexity:  O(n) where n is the number of nodes
     # Space Complexity: O(1)
     def delete(value)
       return if @head.nil?
@@ -160,9 +142,7 @@ class LinkedList
       end
     end
 
-    # method to reverse the singly linked list
-    # note: the nodes should be moved and not just the values in the nodes
-    # Time Complexity:  O(n) where n is the number of nodes
+    # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def reverse
       return nil if @head.nil?
@@ -181,9 +161,6 @@ class LinkedList
 
     end
 
-
-    ## Advanced Exercises
-    # returns the value at the middle element in the singly linked list
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def find_middle_value
@@ -203,8 +180,6 @@ class LinkedList
 
     end
 
-    # find the nth node from the end and return its value
-    # assume indexing starts at 0 while counting to n
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def find_nth_from_end(n)
@@ -220,9 +195,6 @@ class LinkedList
       end
     end
 
-    # checks if the linked list has a cycle. A cycle exists if any node in the
-    # linked list links to a node already visited.
-    # returns true if a cycle is found, false otherwise.
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def has_cycle
@@ -240,9 +212,6 @@ class LinkedList
     end
 
 
-    # Additional Exercises 
-    # returns the value in the first node
-    # returns nil if the list is empty
     # Time Complexity: O(1)
     # Space Complexity: O(1)
     def get_first
@@ -250,8 +219,7 @@ class LinkedList
       return @head.data
     end
 
-    # method that inserts a given value as a new last node in the linked list
-    # Time Complexity:  O(n) where n is the number of nodes
+    # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def add_last(value)
       new_node = Node.new(value)
@@ -276,17 +244,13 @@ class LinkedList
       return value
     end
 
-    # method that returns the value of the last node in the linked list
-    # returns nil if the linked list is empty
-    # Time Complexity:  O(n) where n is the number of nodes
+    # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def get_last
       return nil if @head.nil?
       return @tail.data
     end
 
-    # method to insert a new node with specific data value, assuming the linked
-    # list is sorted in ascending order
     # Time Complexity:  O(n)
     # Space Complexity: O(1)
     def insert_ascending(value)
@@ -313,19 +277,15 @@ class LinkedList
       end
     end
 
-    # Helper method for tests
-    # Creates a cycle in the linked list for testing purposes
-    # Assumes the linked list has at least one node
     def create_cycle
-      return if @head == nil # don't do anything if the linked list is empty
+      return if @head == nil
 
-      # navigate to last node
       current = @head
       while current.next != nil
           current = current.next
       end
 
-      current.next = @head # make the last node link to first node
+      current.next = @head 
     end
 
     def to_s
--- lib/problems.rb
+++ lib/problems.rb
@@ -1,13 +1,47 @@
 require_relative './stack.rb'
+require_relative './queue.rb'
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def balanced(string)
-  raise NotImplementedError, "Not implemented yet"
+  return true if string.empty?
+  
+  stack = Stack.new
+  openers = ["(", "[", "{"]
+  valid_pairs = {
+    "(" => ")",
+    "[" => "]",
+    "{" => "}",
+  }
+  
+  string.each_char do |char|
+    if openers.include?(char)
+      stack.push(char)
+    else
+      popped = stack.pop
+      return false if valid_pairs[popped] != char
+    end
+  end
+  
+  return true if stack.empty?
+  return false
 end
 
-# Time Complexity: ?
-# Space Complexity: ?
+# Time Complexity: O(n)
+# Space Complexity: O(1)
 def evaluate_postfix(postfix_expression)
-  raise NotImplementedError, "Not implemented yet"
+  ops = ["+", "-", "*", "/"]
+  stack = Stack.new
+  
+  postfix_expression.each_char do |char|
+    if !(ops.include?(char))
+      stack.push(char)
+    else
+        num2 = stack.pop
+        num1 = stack.pop
+        expression = "#{num1} #{char} #{num2}"
+        stack.push(eval(expression))
+    end
+  end
+  return stack.pop
 end
